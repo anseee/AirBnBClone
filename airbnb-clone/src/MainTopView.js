ab8a04a01be1787676c700eff19df7f5d8bc4e5a
anseee <admin@ansee.kr>
2018-07-09 23:03:59 +0900

    - change background image at that time

--- airbnb-clone/src/MainTopView.js
+++ airbnb-clone/src/MainTopView.js
@@ -4,9 +4,27 @@ import AirbnbLogo from './image/airbnbLogo.png';
 import SearchIcon from './image/search.png';
 
 class MainTopView extends Component {
+
+    state = {
+        imageURL: "http://awaltrip.com/images/holidayimages/your-worst-family-vacation-stories-ever.jpg"
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            console.log("componentDidMount");
+            this.setState({
+                imageURL: "https://static1.squarespace.com/static/553e4855e4b025fa3cc499d1/t/5728aee301dbae3793d15576/1462284024016/"                
+            })
+        }, 5000);
+    }
+
     render() {
+        var divStyle = {
+            backgroundImage: 'url('+this.state.imageURL+')'
+        }
+
         return (
-            <div className='MainTopView'>
+            <div className='MainTopView' style={divStyle}>
                 <MainTopNavi />
                 <MainTopSpace />
                 <MainTopTextAndSearchBar />
@@ -20,11 +38,11 @@ function MainTopNavi() {
         <nav className='MainTopView__Navi'>
             <div className='MainTopView__Navi__Logo'><a href=""><img src={AirbnbLogo} /></a></div>
             <ul className="MainTopView__Navi__Text">
-                <li><a href="" class="MainTopView__Navi__Main__Text__Links">호스팅하기</a></li>
-                <li><a href="" class="MainTopView__Navi__Main__Text__Links">여행크레딧 받기</a></li>
-                <li><a href="" class="MainTopView__Navi__Main__Text__Links">도움말</a></li>
-                <li><a href="" class="MainTopView__Navi__Main__Text__Links">회원가입</a></li>
-                <li><a href="" class="MainTopView__Navi__Main__Text__Links__Login">로그인</a></li>
+                <li><a href="" className="MainTopView__Navi__Main__Text__Links">호스팅하기</a></li>
+                <li><a href="" className="MainTopView__Navi__Main__Text__Links">여행크레딧 받기</a></li>
+                <li><a href="" className="MainTopView__Navi__Main__Text__Links">도움말</a></li>
+                <li><a href="" className="MainTopView__Navi__Main__Text__Links">회원가입</a></li>
+                <li><a href="" className="MainTopView__Navi__Main__Text__Links__Login">로그인</a></li>
             </ul>
         </nav> 
     ) 
@@ -32,8 +50,7 @@ function MainTopNavi() {
 
 function MainTopSpace() {
     return (
-       <div className="MainTopSpace">
-       </div> 
+       <div className="MainTopSpace"></div> 
     )
 }
 
